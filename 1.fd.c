315103479bd42ec780f1fc7b101809c867b5578f
jiawj12 <thujwj@gmail.com>
2024-05-26 08:54:42 +0800

    ceshi

---
+++ 1.fd.c
@@ -0,0 +1,39 @@
+#include <stdio.h>
+#include <unistd.h>
+#include <stdlib.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <fcntl.h>
+
+
+int main(int argc, char **argv) {
+    int fd, fd2;
+
+    if(argc != 3) {
+        fprintf(stderr, "Usage : %s file1 file2\n", argv[0]);
+        exit(1);
+    }
+    if((fd = open(argv[1], O_RDONLY)) < 0) {
+        perror("open");
+        exit(1);
+    }
+
+    if((fd2 = open(argv[2], O_CREAT | O_RDWR)) < 0) {
+        perror("open");
+        exit(1);
+    }
+
+    lseek(fd, 0, SEEK_SET);
+
+    while(1) {
+        char buff[512] = {0};
+        ssize_t size = read(fd, buff, sizeof(buff));
+        printf("%s", buff);
+        write(fd2, buff, size);
+        if(size <= 0) break;
+    }
+
+    close(fd);
+    close(fd2);
+    return 0;
+}
